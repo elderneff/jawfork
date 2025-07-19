0c7b36769dedcfec1918774039ece2ee3ba89266
elderneff <66916096+elderneff@users.noreply.github.com>
2025-07-19 16:37:27 -0600

    Allow reading in xpt files

--- R/e__load_dataset.R
+++ R/e__load_dataset.R
@@ -49,6 +49,10 @@ e__load_dataset <- function(session_name,outer_env=totem) {
           stringsAsFactors = FALSE
         )      
     }
+    #xpt
+    }else if(tolower(outer_env[[session_name]]$passed_ext)=="xpt"){
+      outer_env[[session_name]]$data1 <- as.data.frame(haven::read_xpt(data_file = outer_env[[session_name]]$sas_file_path))
+      outer_env[[session_name]]$data1_contents <- sas_contents(outer_env[[session_name]]$sas_file_path)
   } else {
     outer_env[[session_name]]$data1 <- as.data.frame(get(x = outer_env[[session_name]]$sas_file_path, envir = .GlobalEnv))
 
