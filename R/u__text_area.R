df70d33c93daf837c739338a369f870fbb117a5a
elderneff <66916096+elderneff@users.noreply.github.com>
2025-09-13 15:20:59 -0600

    Print session_name to troubleshoot

--- R/u__text_area.R
+++ R/u__text_area.R
@@ -24,6 +24,7 @@ u__add_text_area <- function(label, shift_function, session, timeline, time, out
                     ##########################################
                     if(ctrl & single_key %in% c("114")){
                       print("Refresh command received")
+                      print(paste0("session_name: ", session_name))
                       #refresh(session_name)
                       outer_env$show_load_window()
                       outer_env$u__load_dataset(session_name)
