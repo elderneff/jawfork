1c1ed092aaa26217af43810f72d103bd2ef966fc
elderneff <66916096+elderneff@users.noreply.github.com>
2025-12-20 17:04:49 -0700

    copy_full_to_file features

--- R/e__df_obj_function.R
+++ R/e__df_obj_function.R
@@ -147,7 +147,8 @@ e__df_obj_function <- function(box, outer_env = totem,obj_env=inner_env) {
     sas_ext <- unlist(gregexpr('.', sas_w_ext, fixed = T))[1]
     sas_no_ext <- substr(sas_w_ext, 1, sas_ext - 1)
     temp <- paste0(tempdir(), "\\", sas_no_ext, ".csv")
-    write.csv(temp_df, file=temp)
+    userna <-""
+    write.csv(temp_df, file=temp, row.names = F, na = userna)
     shell.exec(temp)
   }
 
