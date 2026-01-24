ce6621340acd1b7a8fd9b8235daad97cfd1f74f0
elderneff <66916096+elderneff@users.noreply.github.com>
2026-01-23 22:54:03 -0700

    Try to generate dialog box for corrupted settings

--- R/jaw.R
+++ R/jaw.R
@@ -11,6 +11,14 @@ jaw <- function(settings_dir=NULL) {
   color_bg_1 <- "#FFFFFF"
   color_bg_2 <- "#f9f9f9"
   totem <- create_initial_list(settings_dir)
+  #Generate error if settings were corrupted
+  dialog <- gtkMessageDialog(
+    parent = totem, 
+    flags = "destroy-with-parent", 
+    type = "question", 
+    buttons = "ok-cancel", 
+    "Select an option for the copied code")
+  gtkWidgetDestroy(dialog)
   totem$while_loop_running <- F
   # Events
   totem$all_event_functions <- (e__with_env(e__all_event_functions))()
