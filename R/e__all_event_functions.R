aafcde253740fe8c0ac809bc782b3df86ed992d4
elderneff <66916096+elderneff@users.noreply.github.com>
2026-02-07 12:59:18 -0700

    Add add_before_filter_table to context menu

--- R/e__all_event_functions.R
+++ R/e__all_event_functions.R
@@ -289,6 +289,10 @@ e__all_event_functions <- function(outer_env = totem) {
     outer_env$u__add_before_filter_full_data_column(session_name, obj_env$table_objects_list$current_row, obj_env$df_obj, exclude = T)
   }
 
+  i__all_event_functions[["Summary Table"]][["Add Table to Main Filter"]] <- function(session_name, current_row, view_objects, outer_env = totem, obj_env = inner_env) {
+    outer_env$u__add_before_filter_table(session_name, obj_env$table_objects_list$current_row, obj_env$df_obj, exclude = T)
+  }
+
   i__all_event_functions[["Summary Table"]][["Add Count to df"]] <- function(session_name, current_row, view_objects, outer_env = totem, obj_env = inner_env) {
     cross_tab_names <- setdiff(colnames(obj_env$table_objects_list$current_row$row), c("r__", "n", "freq", "lines"))
     outer_env$u__add_count_to_df_summary(session_name, cross_tab_names)
