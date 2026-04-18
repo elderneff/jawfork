3742386915771ac84c62b87ad3176000a1021b8e
elderneff <66916096+elderneff@users.noreply.github.com>
2026-04-18 12:08:07 -0600

    Attempt to keep console open when reading a bad dataset

--- R/e__load_dataset.R
+++ R/e__load_dataset.R
@@ -30,16 +30,16 @@ e__load_dataset <- function(session_name,outer_env=totem) {
           message("The file '", outer_env[[session_name]]$sas_file_basename, "' may be corrupted.")
           readline() 
           return(FALSE) # Exit early only on failure
+        } else {      
+          # SUCCESS PATH: Assign data and continue normally
+          print("Line 5: Data read successfully.")
+          Sys.sleep(2)
+          outer_env[[session_name]]$data1 <- as.data.frame(haven::read_sas(data_file = outer_env[[session_name]]$sas_file_path))
+          outer_env[[session_name]]$data1_contents <- sas_contents(outer_env[[session_name]]$sas_file_path)
+          Sys.sleep(2)
+          # No return(F) here! Let it fall through to the update logic.
         }
-      
-        # SUCCESS PATH: Assign data and continue normally
-        print("Line 5: Data read successfully.")
-        Sys.sleep(5)
-        outer_env[[session_name]]$datal <- try_read
-        outer_env[[session_name]]$datal_contents <- sas_contents(outer_env[[session_name]]$sas_file_path)
-        # No return(F) here! Let it fall through to the update logic.
-
-
+        
       
     }
     #sav
