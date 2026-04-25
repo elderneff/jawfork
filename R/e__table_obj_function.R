23b208d0c766e0ed3e908043f44662f4b1fd52bb
elderneff <66916096+elderneff@users.noreply.github.com>
2026-04-25 14:56:13 -0600

    Attempt to implement shift+scrolling

--- R/e__table_obj_function.R
+++ R/e__table_obj_function.R
@@ -238,6 +238,39 @@ e__table_obj_function <- function(box, outer_env = totem,obj_env=inner_env) {
         }
         return(FALSE)
       }, data = obj_env$table_objects_list$view)
+
+      # Shift + Scroll for horizontal scrolling
+      RGtk2::gSignalConnect(obj_env$table_objects_list$view, "scroll-event", function(widget, event, data) {
+        sw_main <- data
+        
+        # Extract the state integer (Shift mask is always bit 1 in GTK)
+        state_int <- as.integer(event[["state"]])
+        is_shift <- bitwAnd(state_int, 1) > 0
+        
+        if (is_shift) {
+          direction <- event[["direction"]]
+          hadj <- RGtk2::gtkScrolledWindowGetHadjustment(sw_main)
+          
+          # Determine scroll amount (multiplying the default step by 3 usually feels natural)
+          step <- hadj$stepIncrement * 3
+          if (is.null(step) || step == 0) step <- 50
+          
+          if (direction == RGtk2::GdkScrollDirection["up"]) {
+            # Scroll Up = Move Left
+            new_val <- max(hadj$lower, hadj$value - step)
+            RGtk2::gtkAdjustmentSetValue(hadj, new_val)
+            return(TRUE) # Return TRUE to tell GTK we handled the event (stops vertical scroll)
+            
+          } else if (direction == RGtk2::GdkScrollDirection["down"]) {
+            # Scroll Down = Move Right
+            new_val <- min(hadj$upper - hadj$pageSize, hadj$value + step)
+            RGtk2::gtkAdjustmentSetValue(hadj, new_val)
+            return(TRUE) # Return TRUE to tell GTK we handled the event (stops vertical scroll)
+          }
+        }
+        
+        return(FALSE) # Return FALSE to let GTK handle normal vertical scrolling
+      }, data = sw)
     } else {
       obj_env$table_objects_list$raw_df <- df
       df2 <- obj_env$table_obj_function_df2(df)
