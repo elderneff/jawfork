c630c5caed5fc1a1b3e3e82c681830d23da779f2
elderneff <66916096+elderneff@users.noreply.github.com>
2026-05-30 13:51:14 -0600

    Make settings saving smarter

--- R/e__close_all_windows.R
+++ R/e__close_all_windows.R
@@ -19,31 +19,33 @@ e__close_all_windows <- function(session_name,outer_env=totem) {
   # outer_env[[session_name]]$dialog$main_content_window$destroy()
 
 
-  fd_allocation <- RGtk2::gtkWidgetGetAllocation(outer_env[[session_name]]$data_view_list$bottom_tables_box)$allocation
-
-
+  # Harvest the current window states
   main_position <- RGtk2::gtkPanedGetPosition(outer_env[[session_name]]$data_view_list$main_paned)
-
-  totem$settings_list$default_sizes$main_pane <- main_position
-
-
   top_position <- RGtk2::gtkPanedGetPosition(outer_env[[session_name]]$data_view_list$top_paned)
-
-  totem$settings_list$default_sizes$top_pane <- top_position
-
-
   slot_position <- RGtk2::gtkPanedGetPosition(outer_env[[session_name]]$data_view_list$paned)
-
-  totem$settings_list$default_sizes$slot_pane <- slot_position
-
-  totem$settings_list$simplicity <- outer_env[[session_name]]$status_bar$simplicity_view
-
+  simplicity_view <- outer_env[[session_name]]$status_bar$simplicity_view
   allocation <- RGtk2::gtkWidgetGetAllocation(outer_env[[session_name]]$windows$main_window)$allocation
-  totem$settings_list$default_sizes$window <- c(allocation$width, allocation$height)
+  window_size <- c(allocation$width, allocation$height)
 
-  #Sys.sleep(10)
+  # Check against the initial snapshot taken at startup
+  initial <- outer_env[[session_name]]$initial_sizes
+
+  if (!is.null(initial)) {
+    # Only overwrite the global settings if THIS specific window was modified
+    if (main_position != initial$main_pane) totem$settings_list$default_sizes$main_pane <- main_position
+    if (top_position != initial$top_pane) totem$settings_list$default_sizes$top_pane <- top_position
+    if (slot_position != initial$slot_pane) totem$settings_list$default_sizes$slot_pane <- slot_position
+    if (simplicity_view != initial$simplicity) totem$settings_list$simplicity <- simplicity_view
+    if (window_size[1] != initial$window[1] || window_size[2] != initial$window[2]) {
+      totem$settings_list$default_sizes$window <- window_size
+    }
+  }
+
+  # Always save settings to disk on close to ensure history/keybinds are preserved
+  save_settings(totem)
+
+  # Destroy the windows
   RGtk2::gtkWidgetDestroy(outer_env[[session_name]]$windows$main_window)
   RGtk2::gtkWidgetDestroy(outer_env[[session_name]]$past_code_window)
   outer_env[[session_name]] <- NULL
-  save_settings(totem)
 }
