27821ca77fc9e8211f300a3a794c390317f7d5f1
elderneff <66916096+elderneff@users.noreply.github.com>
2026-05-30 14:18:47 -0600

    Global settings for code case and operator spacing

--- R/e__copy_if_then.R
+++ R/e__copy_if_then.R
@@ -8,79 +8,102 @@
 #'
 #' @return TODO
 
-e__copy_if_then <- function(session_name, current_row, df_obj,outer_env=totem) {
+e__copy_if_then <- function(session_name, current_row, df_obj, outer_env = totem) {
   require(RGtk2)
   
-  #Generate dialog to ask about code case
-  dialog <- gtkMessageDialog(
-    parent = outer_env[[session_name]]$windows$main_window, 
-    flags = "destroy-with-parent", 
-    type = "question", 
-    buttons = "ok-cancel", 
-    "Select an option for the copied code")
-  
-  #Add options
-  choices <- c("Lowercase", "Uppercase")
-  radio_buttons <- NULL
-  vbox <- gtkVBox(F, 0)
-  for (choice in choices) {
-    button <- gtkRadioButton(radio_buttons, choice)
-    vbox$add(button)
-    radio_buttons <- c(radio_buttons, button)
+  # 1. Check if we need to prompt the user
+  if (is.null(outer_env$settings_list$code_case) || outer_env$settings_list$code_case == "Prompt" ||
+      is.null(outer_env$settings_list$code_spacing) || outer_env$settings_list$code_spacing == "Prompt") {
+    
+    dialog <- gtkMessageDialog(
+      parent = outer_env[[session_name]]$windows$main_window, 
+      flags = "destroy-with-parent", 
+      type = "question", 
+      buttons = "ok-cancel", 
+      "Select preferences for generated code (these can be changed later in Settings):"
+    )
+    
+    # Add Case Options
+    choices_case <- c("Lowercase", "Uppercase")
+    radio_buttons_case <- NULL
+    vbox_case <- gtkVBox(F, 0)
+    for (choice in choices_case) {
+      button <- gtkRadioButton(radio_buttons_case, choice)
+      vbox_case$add(button)
+      radio_buttons_case <- c(radio_buttons_case, button)
+    }
+    frame_case <- gtkFrame("Letter case")
+    frame_case$add(vbox_case)
+    dialog[["vbox"]]$add(frame_case)
+    
+    # Add Spacing Options
+    choices_space <- c("Spaced (x = y)", "Compact (x=y)")
+    radio_buttons_space <- NULL
+    vbox_space <- gtkVBox(F, 0)
+    for (choice in choices_space) {
+      button <- gtkRadioButton(radio_buttons_space, choice)
+      vbox_space$add(button)
+      radio_buttons_space <- c(radio_buttons_space, button)
+    }
+    frame_space <- gtkFrame("Operator Spacing")
+    frame_space$add(vbox_space)
+    dialog[["vbox"]]$add(frame_space)
+    
+    response <- dialog$run()
+    
+    # Escape out if user cancels
+    if (response %in% c(GtkResponseType["close"], GtkResponseType["delete-event"], GtkResponseType["cancel"])) {
+      gtkWidgetDestroy(dialog)
+      return()
+    }
+    
+    # Save selections to Global Settings
+    for (i in 1:length(radio_buttons_case)) {
+      if (gtkToggleButtonGetActive(radio_buttons_case[[i]])) outer_env$settings_list$code_case <- choices_case[i]
+    }
+    for (i in 1:length(radio_buttons_space)) {
+      if (gtkToggleButtonGetActive(radio_buttons_space[[i]])) outer_env$settings_list$code_spacing <- choices_space[i]
+    }
+    
+    gtkWidgetDestroy(dialog)
+    save_settings(outer_env)
   }
   
-  #Make a frame for the buttons
-  frame <- gtkFrame("Letter case")
-  frame$add(vbox)
-  dialog[["vbox"]]$add(frame)
-  #Require response before interacting with table
-  response <- dialog$run()
-
-  #Find selection
-  for (i in 1:length(radio_buttons)) {
-    if (gtkToggleButtonGetActive(radio_buttons[[i]])) {
-      selectn <- i
-    }
+  # 2. Extract preferences
+  c_case <- outer_env$settings_list$code_case
+  c_space <- outer_env$settings_list$code_spacing
+  sp <- ifelse(c_space == "Spaced (x = y)", " = ", "=")
+  
+  column_classes <- df_obj$get_column_classes()
+  column_values <- df_obj$get_column_values(current_row$column)
+  sep <- ifelse(column_classes[current_row$column] == "numeric", "", "\"")
+  
+  string_builder <- rep(NA, length(column_values) + 2)
+  
+  # 3. Generate Code
+  if (toupper(c_case) == "UPPERCASE") {
+    string_builder[1] <- paste0("IF MISSING(", current_row$column, ") THEN VAR", sp, "\"\";")
+  } else {
+    string_builder[1] <- paste0("if missing(", current_row$column, ") then var", sp, "\"\";")
   }
-  selection <- choices[selectn]
   
-  #Destroy dialog box
-  gtkWidgetDestroy(dialog)
-  
-  if (response %in% c(GtkResponseType["close"], GtkResponseType["delete-event"], GtkResponseType["cancel"]) == F) {
-    column_classes <- df_obj$get_column_classes()
-    column_values <- df_obj$get_column_values(current_row$column)
-    if (column_classes[current_row$column] == "numeric") {
-      sep <- ""
+  j <- 2
+  for (i in column_values) {
+    if (toupper(c_case) == "UPPERCASE") {
+      string_builder[j] <- paste0("ELSE IF ", current_row$column, sp, sep, i, sep, " THEN VAR", sp, "\"\";")
     } else {
-      sep <- "\""
+      string_builder[j] <- paste0("else if ", current_row$column, sp, sep, i, sep, " then var", sp, "\"\";")
     }
-  
-    string_builder <- rep(NA, length(column_values) + 2)
-    if (toupper(selection) == "UPPERCASE") {
-      string_builder[1] <- paste0("IF MISSING(", current_row$column, ") THEN <var> = \"\";")
-    } else {
-      string_builder[1] <- paste0("if missing(", current_row$column, ") then <var>=\"\";")
-    }
-    j <- 2
-    for (i in column_values) {
-      if (toupper(selection) == "UPPERCASE") {
-        string_builder[j] <- paste0("ELSE IF ", current_row$column, " = ", sep, i, sep, " THEN <var> = \"\";")
-      } else {
-        string_builder[j] <- paste0("else if ", current_row$column, "=", sep, i, sep, " then <var>=\"\";")
-      }
-  
-      j <- j + 1
-    }
-    if (toupper(selection) == "UPPERCASE") {
-      string_builder[j] <- paste0("ELSE DO;\n    ERR_MSG = CATX(\"|\",\"ERROR: Unexpected value for ", current_row$column, "\", ", current_row$column, ");\n    PUT ERR_MSG;\nEND;")
-    } else {
-      string_builder[j] <- paste0("else do;\n    err_msg=catx(\"|\",\"ERROR: Unexpected value for ", current_row$column, "\", ", current_row$column, ");\n    put err_msg;\nend;")
-    }
-  
-    #Copy final string to clipboard
-    utils::writeClipboard(str = charToRaw(paste0(paste0(string_builder, collapse = "\n"), " ")), format = 1)
+    j <- j + 1
   }
+  
+  if (toupper(c_case) == "UPPERCASE") {
+    string_builder[j] <- paste0("ELSE DO;\n    ERR_MSG", sp, "CATX(\"|\",\"#STUB: Unexpected value for ", current_row$column, "\", ", current_row$column, ");\n    PUT ERR_MSG;\nEND;")
+  } else {
+    string_builder[j] <- paste0("else do;\n    err_msg", sp, "catx(\"|\",\"#STUB: Unexpected value for ", current_row$column, "\", ", current_row$column, ");\n    put err_msg;\nend;")
+  }
+  
+  utils::writeClipboard(str = charToRaw(paste0(paste0(string_builder, collapse = "\n"), " ")), format = 1)
 }
 
 #' e__copy_if_then_do
@@ -92,77 +115,98 @@ e__copy_if_then <- function(session_name, current_row, df_obj,outer_env=totem) {
 #'
 #' @return TODO
 
-e__copy_if_then_do <- function(session_name, current_row, df_obj,outer_env=totem) {
+e__copy_if_then_do <- function(session_name, current_row, df_obj, outer_env = totem) {
   require(RGtk2)
   
-  #Generate dialog to ask about code case
-  dialog <- gtkMessageDialog(
-    parent = outer_env[[session_name]]$windows$main_window, 
-    flags = "destroy-with-parent", 
-    type = "question", 
-    buttons = "ok-cancel", 
-    "Select an option for the copied code")
-  
-  #Add options
-  choices <- c("Lowercase", "Uppercase")
-  radio_buttons <- NULL
-  vbox <- gtkVBox(F, 0)
-  for (choice in choices) {
-    button <- gtkRadioButton(radio_buttons, choice)
-    vbox$add(button)
-    radio_buttons <- c(radio_buttons, button)
+  # 1. Check if we need to prompt the user
+  if (is.null(outer_env$settings_list$code_case) || outer_env$settings_list$code_case == "Prompt" ||
+      is.null(outer_env$settings_list$code_spacing) || outer_env$settings_list$code_spacing == "Prompt") {
+    
+    dialog <- gtkMessageDialog(
+      parent = outer_env[[session_name]]$windows$main_window, 
+      flags = "destroy-with-parent", 
+      type = "question", 
+      buttons = "ok-cancel", 
+      "Select preferences for generated code (these can be changed later in Settings):"
+    )
+    
+    # Add Case Options
+    choices_case <- c("Lowercase", "Uppercase")
+    radio_buttons_case <- NULL
+    vbox_case <- gtkVBox(F, 0)
+    for (choice in choices_case) {
+      button <- gtkRadioButton(radio_buttons_case, choice)
+      vbox_case$add(button)
+      radio_buttons_case <- c(radio_buttons_case, button)
+    }
+    frame_case <- gtkFrame("Letter case")
+    frame_case$add(vbox_case)
+    dialog[["vbox"]]$add(frame_case)
+    
+    # Add Spacing Options
+    choices_space <- c("Spaced (x = y)", "Compact (x=y)")
+    radio_buttons_space <- NULL
+    vbox_space <- gtkVBox(F, 0)
+    for (choice in choices_space) {
+      button <- gtkRadioButton(radio_buttons_space, choice)
+      vbox_space$add(button)
+      radio_buttons_space <- c(radio_buttons_space, button)
+    }
+    frame_space <- gtkFrame("Operator Spacing")
+    frame_space$add(vbox_space)
+    dialog[["vbox"]]$add(frame_space)
+    
+    response <- dialog$run()
+    
+    # Escape out if user cancels
+    if (response %in% c(GtkResponseType["close"], GtkResponseType["delete-event"], GtkResponseType["cancel"])) {
+      gtkWidgetDestroy(dialog)
+      return()
+    }
+    
+    # Save selections to Global Settings
+    for (i in 1:length(radio_buttons_case)) {
+      if (gtkToggleButtonGetActive(radio_buttons_case[[i]])) outer_env$settings_list$code_case <- choices_case[i]
+    }
+    for (i in 1:length(radio_buttons_space)) {
+      if (gtkToggleButtonGetActive(radio_buttons_space[[i]])) outer_env$settings_list$code_spacing <- choices_space[i]
+    }
+    
+    gtkWidgetDestroy(dialog)
+    save_settings(outer_env)
   }
   
-  #Make a frame for the buttons
-  frame <- gtkFrame("Letter case")
-  frame$add(vbox)
-  dialog[["vbox"]]$add(frame)
-  #Require response before interacting with table
-  response <- dialog$run()
-
-  #Find selection
-  for (i in 1:length(radio_buttons)) {
-    if (gtkToggleButtonGetActive(radio_buttons[[i]])) {
-      selectn <- i
-    }
+  # 2. Extract preferences
+  c_case <- outer_env$settings_list$code_case
+  c_space <- outer_env$settings_list$code_spacing
+  sp <- ifelse(c_space == "Spaced (x = y)", " = ", "=")
+  
+  column_classes <- df_obj$get_column_classes()
+  column_values <- df_obj$get_column_values(current_row$column)
+  sep <- ifelse(column_classes[current_row$column] == "numeric", "", "\"")
+  
+  string_builder <- rep(NA, length(column_values) + 2)
+  
+  # 3. Generate Code
+  if (toupper(c_case) == "UPPERCASE") {
+    string_builder[1] <- paste0("IF MISSING(", current_row$column, ") THEN DO;\n    VAR", sp, "\"\";\nEND;")
+  } else {
+    string_builder[1] <- paste0("if missing(", current_row$column, ") then do;\n    var", sp, "\"\";\nend;")
   }
-  selection <- choices[selectn]
-  
-  #Destroy dialog box
-  gtkWidgetDestroy(dialog)
-
-  if (response %in% c(GtkResponseType["close"], GtkResponseType["delete-event"], GtkResponseType["cancel"]) == F) {
-    column_classes <- df_obj$get_column_classes()
-    column_values <- df_obj$get_column_values(current_row$column)
-    if (column_classes[current_row$column] == "numeric") {
-      sep <- ""
+  j <- 2
+  for (i in column_values) {
+    if (toupper(c_case) == "UPPERCASE") {
+      string_builder[j] <- paste0("ELSE IF ", current_row$column, sp, sep, i, sep, " THEN DO;\n    VAR, sp, "\"\";\nEND;")
     } else {
-      sep <- "\""
+      string_builder[j] <- paste0("else if ", current_row$column, sp, sep, i, sep, " then do;\n    var", sp, "\"\";\nend;")
     }
-  
-    string_builder <- rep(NA, length(column_values) + 2)
-    if (toupper(selection) == "UPPERCASE") {
-      string_builder[1] <- paste0("IF MISSING(", current_row$column, ") THEN DO;\n    <var> = \"\";\nEND;")
-    } else {
-      string_builder[1] <- paste0("if missing(", current_row$column, ") then do;\n    <var>=\"\";\nend;")
-    }
-    j <- 2
-    for (i in column_values) {
-      if (toupper(selection) == "UPPERCASE") {
-        string_builder[j] <- paste0("ELSE IF ", current_row$column, " = ", sep, i, sep, " THEN DO;\n    <var> = \"\";\nEND;")
-      } else {
-        string_builder[j] <- paste0("else if ", current_row$column, "=", sep, i, sep, " then do;\n    <var>=\"\";\nend;")
-      }
-  
-      j <- j + 1
-    }
-    if (toupper(selection) == "UPPERCASE") {
-      string_builder[j] <- paste0("ELSE DO;\n    ERR_MSG = CATX(\"|\",\"ERROR: Unexpected value for ", current_row$column, "\", ", current_row$column, ");\n    PUT ERR_MSG;\nEND;")
-    } else {
-      string_builder[j] <- paste0("else do;\n    err_msg=catx(\"|\",\"ERROR: Unexpected value for ", current_row$column, "\", ", current_row$column, ");\n    put err_msg;\nend;")
-    }  
-
-    #Copy final string to clipboard
-    utils::writeClipboard(str = charToRaw(paste0(paste0(string_builder, collapse = "\n"), " ")), format = 1)
+    j <- j + 1
   }
+  if (toupper(c_case) == "UPPERCASE") {
+    string_builder[j] <- paste0("ELSE DO;\n    ERR_MSG", sp, "CATX(\"|\",\"#STUB: Unexpected value for ", current_row$column, "\", ", current_row$column, ");\n    PUT ERR_MSG;\nEND;")
+  } else {
+    string_builder[j] <- paste0("else do;\n    err_msg", sp, "catx(\"|\",\"#STUB: Unexpected value for ", current_row$column, "\", ", current_row$column, ");\n    put err_msg;\nend;")
+  }
+  
+  utils::writeClipboard(str = charToRaw(paste0(paste0(string_builder, collapse = "\n"), " ")), format = 1)
 }
