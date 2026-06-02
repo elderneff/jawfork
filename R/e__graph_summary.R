e4c03d25252778f334b87b1e6f37694f42c2d151
elderneff <66916096+elderneff@users.noreply.github.com>
2026-06-02 12:40:25 -0600

    Revamp summarization functions

--- R/e__graph_summary.R
+++ R/e__graph_summary.R
@@ -7,6 +7,10 @@
 #' @return TODO
 
 e__graph_summary <- function(session_name, current_row,outer_env=totem) {
+  # Trigger loading screen. on.exit ensures it hides even if the function errors out early.
+  outer_env$show_load_window()
+  on.exit(try({ outer_env$hide_load_window() }, silent = TRUE), add = TRUE)
+
   temp_df <- outer_env[[session_name]]$data2
   temp_df$random_char_str <- "a"
   temp_df2 <- as.matrix(temp_df)
@@ -28,7 +32,7 @@ e__graph_summary <- function(session_name, current_row,outer_env=totem) {
   ################################################################
   
   # ERROR CATCH: Ensure the selected column is numeric
-  if (!(class(temp_df[[current_row$column]]) %in% c("numeric", "integer"))) {
+  if (!any(class(temp_df[[current_row$column]]) %in% c("numeric", "integer"))) {
     err_dialog <- RGtk2::gtkMessageDialog(
       parent = outer_env[[session_name]]$windows$main_window, 
       flags = "destroy-with-parent", 
@@ -53,12 +57,37 @@ e__graph_summary <- function(session_name, current_row,outer_env=totem) {
 
   ### Handle when there are grouping variables ###
   if (group_by_entry != "") {
+    
+    # 1. Prepare for an overall group
+    group_vars <- stringr::str_split(group_by_entry, ", ")[[1]]
+    temp_df_combined <- temp_df
+    
+    # Convert grouping vars to character to prevent factor level warnings
+    for (g in group_vars) {
+      if (is.factor(temp_df_combined[[g]])) {
+        temp_df_combined[[g]] <- as.character(temp_df_combined[[g]])
+      }
+    }
+    
+    # 2. Duplicate data and assign a safe "OVERALL" category
+    temp_df_overall <- temp_df_combined
+    for (g in group_vars) {
+      safe_overall <- "OVERALL"
+      while (safe_overall %in% temp_df_combined[[g]]) {
+        safe_overall <- paste0(safe_overall, " ")
+      }
+      temp_df_overall[[g]] <- safe_overall
+    }
+    temp_df_combined <- rbind(temp_df_combined, temp_df_overall)
+
     #Separate group_by_entry by asterisks instead of commas
     group_by_entry_asterisks <- gsub(", ", "*", group_by_entry)
     #Concatenate the independent and dependent variables into a string
     title <- sprintf('%s ~ %s', current_row$column, group_by_entry_asterisks)
-    eval(parse(text = sprintf('plot <- boxplot(%s ~ %s, temp_df, ylab = "%s", main = "%s")', current_row$column, group_by_entry_asterisks, current_row$column, title)))
-    text(1:length(plot$n), min(temp_df[[current_row$column]], na.rm = T) - ((max(temp_df[[current_row$column]], na.rm = T) - min(temp_df[[current_row$column]], na.rm = T)) * 0.02), paste("n=", plot$n))
+    
+    # 3. Plot using the combined dataframe
+    eval(parse(text = sprintf('plot <- boxplot(%s ~ %s, temp_df_combined, ylab = "%s", main = "%s")', current_row$column, group_by_entry_asterisks, current_row$column, title)))
+    text(1:length(plot$n), min(temp_df_combined[[current_row$column]], na.rm = T) - ((max(temp_df_combined[[current_row$column]], na.rm = T) - min(temp_df_combined[[current_row$column]], na.rm = T)) * 0.02), paste("n=", plot$n))
     grid()
     
   ### Handle when there are no grouping variables ###
