a6aea588d2b90d3d81d3cd5350f32fd768e6a29c
elderneff <66916096+elderneff@users.noreply.github.com>
2026-06-03 16:12:10 -0600

    Dark mode

--- R/e__table_obj_function.R
+++ R/e__table_obj_function.R
@@ -182,11 +182,11 @@ e__table_obj_function_df2 <- function(df, outer_env = totem, obj_env = inner_env
     if (has_filter || has_arrange || has_select) {
       df2[, 1] <- ifelse(is_dark, "#5C2E2E", "#F4D9D9")
     } else {
-      if (is_dark) {
-        df2[, 1] <- "#1A365D"
-      } else {
+      #if (is_dark) {
+      #  df2[, 1] <- "#1A365D"
+      #} else {
         df2[, 1] <- "#9bb5f5"
-      }
+      #}
     }
 
   colnames(df2) <- c("f___1", "f___2")
