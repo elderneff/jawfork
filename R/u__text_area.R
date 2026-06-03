a014a7506cfc3a949cf7e72c7b1b8d0dbfc5f606
elderneff <66916096+elderneff@users.noreply.github.com>
2026-06-03 14:25:09 -0600

    Ignore caps lock and num lock

--- R/u__text_area.R
+++ R/u__text_area.R
@@ -50,86 +50,93 @@ u__add_text_area <- function(label, shift_function, session, outer_env) {
                   session<- data[[1]]
                   shift_function <- data[[2]]
                   outer_env <- data[[3]]
-
-                    #Clean up the key state to ignore Caps/Num Lock keys
-                    raw_state <- as.numeric(event[["state"]])
-                    lock_masks <- bitwOr(2, 16) # Caps Lock = 2, Num Lock = 16
-                    clean_state <- bitwAnd(raw_state, bitwNot(lock_masks))
                   
-                    ###################################
-                    # Run code for select key strokes #
-                    ###################################
-                    key_state <- z__event_state_key(event)
-                    single_key <- event[["keyval"]]
-                    ctrl <- clean_state == 4
+                  # --- FIX: Clean up the key state to ignore Caps/Num Lock keys
+                  raw_state <- as.numeric(event[["state"]])
+                  lock_masks <- bitwOr(2, 16) # Caps Lock = 2, Num Lock = 16
+                  clean_state <- bitwAnd(raw_state, bitwNot(lock_masks))
                   
-                    run_triggered <- FALSE                    
-                    #Look for Ctrl+Enter
-                    if (ctrl & single_key %in% c("65293", "65458")) {
-                        run_triggered <- TRUE
-                    }                    
-                    #Look for Ctrl+Shift, only if not cancelled by other keys
-                    if (key_state == "shift+ctrl" & outer_env$settings_list$ctrlshift) {
-                        if (!isTRUE(outer_env[[session]]$cancel_ctrl_shift)) {
+                  ###################################
+                  # Run code for select key strokes #
+                  ###################################
+                  key_state <- z__event_state_key(event)
+                  single_key <- event[["keyval"]]
+                  
+                  # Update the ctrl check to look at our sanitized clean_state instead of event[["state"]] == "4"
+                  ctrl <- clean_state == 4
+                  
+                  run_triggered <- FALSE                   
+                  
+                  #Look for Ctrl+Enter
+                  if (ctrl & single_key %in% c("65293", "65458")) {
+                      run_triggered <- TRUE
+                  }    
+                                   
+                  #Look for Ctrl+Shift, only if not cancelled by other keys
+                  if (key_state == "shift+ctrl" & outer_env$settings_list$ctrlshift) {
+                      if (!isTRUE(outer_env[[session]]$cancel_ctrl_shift)) {
                             run_triggered <- TRUE
-                        }
-                    }                    
-                    #Run for either condition
-                    if (run_triggered) {
-                        shift_function(session)
-                    }
-                    ##########################################
-                    # Refresh dataset for select key strokes #
-                    ##########################################
-                    if(ctrl & single_key %in% c("114")){
-                      outer_env$show_load_window()
-                      outer_env$u__load_dataset(session)
-                      outer_env$hide_load_window()
-                    }
-                    ##########################################
-                    # Store buffer whenever a key is pressed #
-                    ##########################################
-                    buffer <- RGtk2::gtkTextViewGetBuffer(view)
-                    end_iter <- RGtk2::gtkTextBufferGetEndIter(buffer)
-                    start_iter <- RGtk2::gtkTextBufferGetStartIter(buffer)
-                    str <- RGtk2::gtkTextBufferGetText(buffer,
-                      start_iter$iter, end_iter$iter,
-                      include.hidden.chars = TRUE
-                    )
-                    #########################
-                    # Undo / Redo Tracking  #
-                    #########################
-                    if (!(single_key %in% c("65507", "65505", "65513", "16777215", "65506", "65508", "65514", "65361", "65362", "65363", "65364", "65360", "65367", "65289"))
-                         & !(single_key == "122" & ctrl) & !(single_key == "121" & ctrl) & !(single_key == "114" & ctrl)) {
-                      
-                      # Categorize the keystroke
-                      current_state <- "word"
-                      if (single_key == "32") current_state <- "space"
-                      else if (single_key %in% c("65293", "65458")) current_state <- "enter"
-                      else if (single_key %in% c("65288", "65535")) current_state <- "delete"
-                      
-                      # Send to universal tracker
-                      outer_env$u__log_history(session, str, current_state)
-                    }
-
-                    # Undo (Ctrl+Z)
-                    if (single_key == "122" & ctrl & outer_env[[session]]$time > 1) {
-                      t <- outer_env[[session]]$time - 1
-                      outer_env[[session]]$time <- t
-                      outer_env[[session]]$last_edit_state <- "undo"
-                      RGtk2::gtkTextBufferSetText(buffer, outer_env[[session]]$timeline[t])
-                    }
-                    
-                    # Redo (Ctrl+Y)
-                    if (single_key == "121" & ctrl & outer_env[[session]]$time < length(outer_env[[session]]$timeline)) {
-                      t <- outer_env[[session]]$time + 1
-                      outer_env[[session]]$time <- t
-                      outer_env[[session]]$last_edit_state <- "redo"
-                      RGtk2::gtkTextBufferSetText(buffer, outer_env[[session]]$timeline[t])
-                    }
+                      }
+                  }                    
+                            
+                  #Run for either condition
+                  if (run_triggered) {
+                      shift_function(session)
+                  }
                   
-                    return(TRUE)
-                },data=list(session,shift_function, outer_env))
+                  ##########################################
+                  # Refresh dataset for select key strokes #
+                  ##########################################
+                  if(ctrl & single_key %in% c("114")){
+                    outer_env$show_load_window()
+                    outer_env$u__load_dataset(session)
+                    outer_env$hide_load_window()
+                  }
+                  ##########################################
+                  # Store buffer whenever a key is pressed #
+                  ##########################################
+                  buffer <- RGtk2::gtkTextViewGetBuffer(view)
+                  end_iter <- RGtk2::gtkTextBufferGetEndIter(buffer)
+                  start_iter <- RGtk2::gtkTextBufferGetStartIter(buffer)
+                  str <- RGtk2::gtkTextBufferGetText(buffer,
+                    start_iter$iter, end_iter$iter,
+                    include.hidden.chars = TRUE
+                  )
+                  
+                  #########################
+                  # Undo / Redo Tracking  #
+                  #########################
+                  if (!(single_key %in% c("65507", "65505", "65513", "16777215", "65506", "65508", "65514", "65361", "65362", "65363", "65364", "65360", "65367", "65289"))
+                     & !(single_key == "122" & ctrl) & !(single_key == "121" & ctrl) & !(single_key == "114" & ctrl)) {
+                    
+                    # Categorize the keystroke
+                    current_state <- "word"
+                    if (single_key == "32") current_state <- "space"
+                    else if (single_key %in% c("65293", "65458")) current_state <- "enter"
+                    else if (single_key %in% c("65288", "65535")) current_state <- "delete"
+              
+                    # Send to universal tracker
+                    outer_env$u__log_history(session, str, current_state)
+                  }
+
+                  # Undo (Ctrl+Z) - Now safely using our fixed 'ctrl' evaluation
+                  if (single_key == "122" & ctrl & outer_env[[session]]$time > 1) {
+                    t <- outer_env[[session]]$time - 1
+                    outer_env[[session]]$time <- t
+                    outer_env[[session]]$last_edit_state <- "undo"
+                    RGtk2::gtkTextBufferSetText(buffer, outer_env[[session]]$timeline[t])
+                  }
+                  
+                  # Redo (Ctrl+Y) - Now safely using our fixed 'ctrl' evaluation
+                  if (single_key == "121" & ctrl & outer_env[[session]]$time < length(outer_env[[session]]$timeline)) {
+                    t <- outer_env[[session]]$time + 1
+                    outer_env[[session]]$time <- t
+                    outer_env[[session]]$last_edit_state <- "redo"
+                    RGtk2::gtkTextBufferSetText(buffer, outer_env[[session]]$timeline[t])
+                  }
+                
+                  return(TRUE)
+                }, data=list(session, shift_function, outer_env))
 
   # Enable Horizontal Scrolling in the Text Area
   RGtk2::gSignalConnect(temp_list$View, "scroll-event", function(widget, event, data) {
