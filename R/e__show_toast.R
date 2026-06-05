63e84eb303f830fb875ca328753111b05b5fda22
elderneff <66916096+elderneff@users.noreply.github.com>
2026-06-05 16:26:21 -0600

    Popup on successful copy

--- R/e__show_toast.R
+++ R/e__show_toast.R
@@ -7,25 +7,30 @@
 #'
 #' @return TODO
 
-e__show_toast <- function(session_name, message = "Code copied to clipboard!", duration_ms = 2000, outer_env = totem) {
+e__show_toast <- function(session_name, message = "Code copied to clipboard!", duration_ms = 1000, outer_env = totem) {
   toast_win <- RGtk2::gtkWindowNew("toplevel")
   RGtk2::gtkWindowSetDecorated(toast_win, FALSE)
   RGtk2::gtkWindowSetResizable(toast_win, FALSE)
   RGtk2::gtkWindowSetKeepAbove(toast_win, TRUE)
-  
-  # Set position policy to NONE (0L) so GTK obeys our math
   RGtk2::gtkWindowSetPosition(toast_win, 0L)
+  
+  # 1. Use an EventBox to force the background color, overriding Dark/Light mode
+  event_box <- RGtk2::gtkEventBoxNew()
+  bg_color <- RGtk2::gdkColorParse("#ffaec8")$color
+  RGtk2::gtkWidgetModifyBg(event_box, RGtk2::GtkStateType["normal"], bg_color)
+  RGtk2::gtkContainerAdd(toast_win, event_box)
   
   frame <- RGtk2::gtkFrameNew()
   RGtk2::gtkFrameSetShadowType(frame, "out")
-  RGtk2::gtkContainerAdd(toast_win, frame)
+  RGtk2::gtkContainerAdd(event_box, frame)
   
   vbox <- RGtk2::gtkVBoxNew(FALSE, 0)
   RGtk2::gtkContainerSetBorderWidth(vbox, 15)
   RGtk2::gtkContainerAdd(frame, vbox)
   
+  # 2. Force text color to black (#000000) using markup
   label <- RGtk2::gtkLabelNew()
-  markup <- paste0("<span size='large' weight='bold' foreground='#2e8b57'>", message, "</span>")
+  markup <- paste0("<span size='large' weight='bold' foreground='#000000'>", message, "</span>")
   RGtk2::gtkLabelSetMarkup(label, markup)
   RGtk2::gtkBoxPackStart(vbox, label, TRUE, TRUE, 0)
   
@@ -35,21 +40,24 @@ e__show_toast <- function(session_name, message = "Code copied to clipboard!", d
     true_parent <- RGtk2::gtkWidgetGetToplevel(parent_window)
     RGtk2::gtkWindowSetTransientFor(toast_win, true_parent)
     
-    # Pre-calculate toast dimensions
-    req <- RGtk2::gtkWidgetSizeRequest(toast_win)$requisition
-    toast_w <- req$width
-    toast_h <- req$height
+    # Pre-calculate toast dimensions (using the tail trick to strip C-level flags)
+    req <- RGtk2::gtkWidgetSizeRequest(toast_win)
+    req_vec <- tail(as.numeric(unlist(req)), 2)
+    toast_w <- req_vec[1]
+    toast_h <- req_vec[2]
     
-    # Get true parent dimensions safely
+    # 3. Bulletproof coordinate extraction
     p_pos <- RGtk2::gtkWindowGetPosition(true_parent)
     p_size <- RGtk2::gtkWindowGetSize(true_parent)
     
-    # Calculate exact bottom-right placement
-    padding <- 50
-    target_x <- p_pos$root_x + p_size$width - toast_w - padding
-    target_y <- p_pos$root_y + p_size$height - toast_h - padding
+    p_vec <- tail(as.numeric(unlist(p_pos)), 2)
+    s_vec <- tail(as.numeric(unlist(p_size)), 2)
     
-    # Move it
+    # Calculate bottom-right placement
+    padding <- 50
+    target_x <- p_vec[1] + s_vec[1] - toast_w - padding
+    target_y <- p_vec[2] + s_vec[2] - toast_h - padding
+    
     RGtk2::gtkWindowMove(toast_win, as.integer(target_x), as.integer(target_y))
   } else {
     RGtk2::gtkWindowSetPosition(toast_win, 1L) 
