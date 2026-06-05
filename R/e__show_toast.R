7d49e9e35ca0fc404266c408c7a3f42694f1abd4
elderneff <66916096+elderneff@users.noreply.github.com>
2026-06-05 16:09:23 -0600

    Pop

--- R/e__show_toast.R
+++ R/e__show_toast.R
@@ -7,25 +7,22 @@
 #'
 #' @return TODO
 
-e__show_toast <- function(session_name, message = "Code copied to clipboard!", duration_ms = 5000, outer_env = totem) {
+e__show_toast <- function(session_name, message = "Code copied to clipboard!", duration_ms = 1000, outer_env = totem) {
   toast_win <- RGtk2::gtkWindowNew("toplevel")
   RGtk2::gtkWindowSetDecorated(toast_win, FALSE)
-  
-  # 1. Shrink-wrap the window so it perfectly hugs the text
   RGtk2::gtkWindowSetResizable(toast_win, FALSE)
+  
+  # 1. Force the window to float on top of everything else
+  RGtk2::gtkWindowSetKeepAbove(toast_win, TRUE)
   
   parent_window <- outer_env[[session_name]]$windows$main_window
   
   if (!is.null(parent_window)) {
-    # 2. Guarantee we have the absolute root window of the application
     true_parent <- RGtk2::gtkWidgetGetToplevel(parent_window)
     RGtk2::gtkWindowSetTransientFor(toast_win, true_parent)
-    
-    # 3. Use the raw C-integer to guarantee placement (4 = GTK_WIN_POS_CENTER_ON_PARENT)
-    RGtk2::gtkWindowSetPosition(toast_win, 4)
+    RGtk2::gtkWindowSetPosition(toast_win, 4) # Center on parent
   } else {
-    # (1 = GTK_WIN_POS_CENTER)
-    RGtk2::gtkWindowSetPosition(toast_win, 1)
+    RGtk2::gtkWindowSetPosition(toast_win, 1) # Center on screen
   }
   
   frame <- RGtk2::gtkFrameNew()
@@ -37,12 +34,15 @@ e__show_toast <- function(session_name, message = "Code copied to clipboard!", d
   RGtk2::gtkContainerAdd(frame, vbox)
   
   label <- RGtk2::gtkLabelNew()
-  # Optional: I added 'center' alignment to the markup just in case
   markup <- paste0("<span size='large' weight='bold' foreground='#2e8b57'>", message, "</span>")
   RGtk2::gtkLabelSetMarkup(label, markup)
   RGtk2::gtkBoxPackStart(vbox, label, TRUE, TRUE, 0)
   
+  # Show the window and all its children
   RGtk2::gtkWidgetShowAll(toast_win)
+  
+  # 2. Forcefully command the window manager to draw it right now
+  RGtk2::gtkWindowPresent(toast_win)
   
   RGtk2::gTimeoutAdd(duration_ms, function(...) {
     RGtk2::gtkWidgetDestroy(toast_win)
