f170045c5373f24cceef0e03581360817c403af8
elderneff <66916096+elderneff@users.noreply.github.com>
2026-06-05 16:35:26 -0600

    Popup on successful copy

--- R/e__show_toast.R
+++ R/e__show_toast.R
@@ -14,6 +14,7 @@ e__show_toast <- function(session_name, message = "Code copied to clipboard!", d
   RGtk2::gtkWindowSetKeepAbove(toast_win, TRUE)
   RGtk2::gtkWindowSetPosition(toast_win, 0L)
   
+  # 1. Custom Pink Background (#ffaec8)
   event_box <- RGtk2::gtkEventBoxNew()
   bg_color <- RGtk2::gdkColorParse("#ffaec8")$color
   RGtk2::gtkWidgetModifyBg(event_box, RGtk2::GtkStateType["normal"], bg_color)
@@ -27,6 +28,7 @@ e__show_toast <- function(session_name, message = "Code copied to clipboard!", d
   RGtk2::gtkContainerSetBorderWidth(vbox, 15)
   RGtk2::gtkContainerAdd(frame, vbox)
   
+  # 2. Custom Black Text
   label <- RGtk2::gtkLabelNew()
   markup <- paste0("<span size='large' weight='bold' foreground='#000000'>", message, "</span>")
   RGtk2::gtkLabelSetMarkup(label, markup)
@@ -38,17 +40,27 @@ e__show_toast <- function(session_name, message = "Code copied to clipboard!", d
     true_parent <- RGtk2::gtkWidgetGetToplevel(parent_window)
     RGtk2::gtkWindowSetTransientFor(toast_win, true_parent)
     
+    # 3. Request the dimensions of our newly built toast
+    req <- RGtk2::gtkWidgetSizeRequest(toast_win)$requisition
+    toast_w <- req$width
+    toast_h <- req$height
+    
+    # 4. Pull the exact variables using the DOT syntax we found in the diagnostic
     p_pos <- RGtk2::gtkWindowGetPosition(true_parent)
     p_size <- RGtk2::gtkWindowGetSize(true_parent)
     
-    # --- DIAGNOSTIC PRINTS ---
-    cat("\n--- WINDOW POSITION STRUCTURE ---\n")
-    str(p_pos)
+    parent_x <- p_pos$root.x
+    parent_y <- p_pos$root.y
+    parent_w <- p_size$width
+    parent_h <- p_size$height
     
-    cat("\n--- WINDOW SIZE STRUCTURE ---\n")
-    str(p_size)
-    cat("---------------------------------\n\n")
+    # 5. Calculate bottom-right placement
+    padding <- 50
+    target_x <- parent_x + parent_w - toast_w - padding
+    target_y <- parent_y + parent_h - toast_h - padding
     
+    # Move it!
+    RGtk2::gtkWindowMove(toast_win, as.integer(target_x), as.integer(target_y))
   } else {
     RGtk2::gtkWindowSetPosition(toast_win, 1L) 
   }
