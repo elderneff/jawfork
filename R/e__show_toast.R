bef470e0a4ec12c528f6e1bfa7331cd10b4dca2b
elderneff <66916096+elderneff@users.noreply.github.com>
2026-06-05 15:59:09 -0600

    Popup on successful copy

--- R/e__show_toast.R
+++ R/e__show_toast.R
@@ -1,27 +1,30 @@
 #' e__show_toast
 #'
-#' @param outer_env TODO
 #' @param session_name TODO
 #' @param message The text to display
 #' @param duration_ms Time in milliseconds before the toast disappears
+#' @param outer_env TODO
 #'
 #' @return TODO
 
-e__show_toast <- function(session_name, message = "Code copied to clipboard!", duration_ms = 2000, outer_env = totem) {
-  # 1. Create a borderless popup window
-  toast_win <- RGtk2::gtkWindowNew("popup")
+e__show_toast <- function(session_name, message = "Code copied to clipboard!", duration_ms = 1000, outer_env = totem) {
+  # 1. Create a TOPLEVEL window (Popups ignore position commands!)
+  toast_win <- RGtk2::gtkWindowNew("toplevel")
+  
+  # Strip away the title bar and X button to make it a frameless toast
+  RGtk2::gtkWindowSetDecorated(toast_win, FALSE)
   
   parent_window <- outer_env[[session_name]]$windows$main_window
   
-  # FIX: Use the explicit RGtk2 enum for window positions instead of raw strings
+  # 2. Position it relative to the parent using the simple strings
   if (!is.null(parent_window)) {
     RGtk2::gtkWindowSetTransientFor(toast_win, parent_window)
-    RGtk2::gtkWindowSetPosition(toast_win, RGtk2::GtkWindowPosition["center-on-parent"])
+    RGtk2::gtkWindowSetPosition(toast_win, "center-on-parent")
   } else {
-    RGtk2::gtkWindowSetPosition(toast_win, RGtk2::GtkWindowPosition["center"])
+    RGtk2::gtkWindowSetPosition(toast_win, "center")
   }
   
-  # 2. Add a decorative frame and padding so it looks like a nice notification
+  # 3. Add a decorative frame and padding so it looks like a nice notification
   frame <- RGtk2::gtkFrameNew()
   RGtk2::gtkFrameSetShadowType(frame, "out")
   RGtk2::gtkContainerAdd(toast_win, frame)
@@ -30,7 +33,7 @@ e__show_toast <- function(session_name, message = "Code copied to clipboard!", d
   RGtk2::gtkContainerSetBorderWidth(vbox, 15)
   RGtk2::gtkContainerAdd(frame, vbox)
   
-  # 3. Add the message label with Pango markup for styling
+  # 4. Add the message label with Pango markup for styling
   label <- RGtk2::gtkLabelNew()
   markup <- paste0("<span size='large' weight='bold' foreground='#2e8b57'>", message, "</span>")
   RGtk2::gtkLabelSetMarkup(label, markup)
@@ -39,7 +42,7 @@ e__show_toast <- function(session_name, message = "Code copied to clipboard!", d
   # Show the window
   RGtk2::gtkWidgetShowAll(toast_win)
   
-  # 4. Set the auto-destruct timer
+  # 5. Set the auto-destruct timer
   RGtk2::gTimeoutAdd(duration_ms, function(...) {
     RGtk2::gtkWidgetDestroy(toast_win)
     return(FALSE) # Returning FALSE tells GTK not to repeat the timer
