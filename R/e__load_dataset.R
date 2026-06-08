1f5d9ea6abc4063ba0fc8cbf7af41e7e29e47258
elderneff <66916096+elderneff@users.noreply.github.com>
2026-06-08 11:14:30 -0600

    Rework past code

--- R/e__load_dataset.R
+++ R/e__load_dataset.R
@@ -340,11 +340,12 @@ e__load_dataset_filter_inner <- function(session_name,outer_env=totem) {
         ), totem$settings_list$previous_code)
 
         previous_code <- previous_code[duplicated(previous_code[, -1]) == F, ]
+
+        # CAP AT 500 ENTRIES
+        previous_code <- head(previous_code, 500)
+        
         totem$settings_list$previous_code <- previous_code
         outer_env[[session_name]]$past_code_window_table$update(previous_code)
-
-
-
 
         return(outer_env$u__load_dataset_filter_inner_select(session_name, outer_env[[session_name]]$e$df))
       },
