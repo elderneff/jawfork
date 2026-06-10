acdf96b8089e9b24cee521f67b6a4aa649da61d1
elderneff <66916096+elderneff@users.noreply.github.com>
2026-06-10 16:33:50 -0600

    Rework file history

--- R/e__file_history.R
+++ R/e__file_history.R
@@ -18,14 +18,12 @@ e__file_history <- function(outer_env=totem) {
 
   RGtk2::gtkBoxPackStart(outer_env$file_history$file_history_window_main_box, outer_env$file_history$file_history_window_main_new_path_box, F, F)
 
-  outer_env$file_history$file_history_window_main_new_path_chk_btn <- RGtk2::gtkButton()
-  RGtk2::gtkContainerAdd(outer_env$file_history$file_history_window_main_new_path_chk_btn, RGtk2::gtkImageNewFromStock("gtk-refresh", size = 2L))
-  RGtk2::gtkWidgetSetTooltipText(outer_env$file_history$file_history_window_main_new_path_chk_btn, "Check if files have been modified on disk")
+  outer_env$file_history$file_history_window_main_new_path_chk_btn <- RGtk2::gtkButton("Check")
   RGtk2::gtkButtonSetFocusOnClick(outer_env$file_history$file_history_window_main_new_path_chk_btn, F)
   RGtk2::gtkBoxPackStart(outer_env$file_history$file_history_window_main_new_path_box, outer_env$file_history$file_history_window_main_new_path_chk_btn, F, F, padding = 5)
 
 
-  RGtk2::gtkBoxPackStart(outer_env$file_history$file_history_window_main_new_path_box, RGtk2::gtkLabel("Path to file: "), F, F, padding = 5)
+  RGtk2::gtkBoxPackStart(outer_env$file_history$file_history_window_main_new_path_box, RGtk2::gtkLabel("New Path"), F, F, padding = 5)
 
   outer_env$file_history$file_history_window_main_new_path_entry <- RGtk2::gtkEntry()
   RGtk2::gtkBoxPackStart(outer_env$file_history$file_history_window_main_new_path_box, outer_env$file_history$file_history_window_main_new_path_entry, T, T, padding = 1)
@@ -35,6 +33,29 @@ e__file_history <- function(outer_env=totem) {
   RGtk2::gtkBoxPackStart(outer_env$file_history$file_history_window_main_new_path_box, outer_env$file_history$file_history_window_main_new_path_btn, F, F, padding = 5)
 
 
+
+
+  RGtk2::gSignalConnect(outer_env$file_history$file_history_window_main_new_path_chk_btn, "button-press-event",
+    function(widget, event, data) {
+      outer_env$show_load_window()
+      outer_env <- data
+      file_history <- outer_env$settings_list$file_history
+      for (i in seq_len(nrow(file_history))) {
+        file_history[i, "latest"] <- NA
+        try({
+          file_history[i, "latest"] <- (file_history[i, "mtime"] == file.info(file_history[i, "full_path"], extra_cols = TRUE)$mtime)
+        })
+      }
+
+
+      outer_env$settings_list$file_history <- file_history
+      outer_env$file_history$file_history_window_table$update(file_history)
+      outer_env$hide_load_window()
+
+      return(FALSE)
+    },
+    data = outer_env
+  )
 
 
   RGtk2::gSignalConnect(outer_env$file_history$file_history_window_main_new_path_chk_btn, "button-press-event",
@@ -84,3 +105,30 @@ e__file_history <- function(outer_env=totem) {
     outer_env$hide_load_window()
     return(FALSE)
   }, data = outer_env)
+
+
+
+
+
+
+  RGtk2::gtkWindowSetTitle(outer_env$file_history$file_history_window, "File History")
+  RGtk2::gtkWidgetSetSizeRequest(outer_env$file_history$file_history_window, 600, 600)
+
+
+  RGtk2::gSignalConnect(outer_env$file_history$file_history_window, "delete-event", f = function(window, event, data) {
+    outer_env <- data
+    outer_env$hide_file_history_window()
+    return(T)
+  }, data = outer_env)
+
+
+
+  outer_env$file_history$file_history_window_table <- outer_env$u__df_tree(
+    session_name = "file_history",
+    passed_box = outer_env$file_history$file_history_window_main_box,
+    rows_length = 1000,
+    event_mapping = NULL,
+    style_list = list(value = RGtk2::pangoFontDescriptionFromString("bold 10")),
+    is_value_table = F, is_meta_table = F, is_data_code_table = F, is_file_history_table = T
+  )
+}
