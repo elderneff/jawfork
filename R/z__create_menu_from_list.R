8c0ccc9199871c336fa0b0cf54f41829fcaba1fd
elderneff <66916096+elderneff@users.noreply.github.com>
2026-07-25 12:12:34 -0600

    Alternating colors in right click menu

--- R/z__create_menu_from_list.R
+++ R/z__create_menu_from_list.R
@@ -7,7 +7,7 @@
 #' @return TODO
 
 z__create_menu_from_list <- function(obj, parent_name, my_list) {
-  # Define items that should have the darker grey background.
+  #Define items that should have the darker grey background.
   dark_items <- c(
     "if then", "if then do",
     "Column full", "Column filtered", "Column Wide",
@@ -15,65 +15,24 @@ z__create_menu_from_list <- function(obj, parent_name, my_list) {
     "Add Bucket to Main Filter Exclude"
   )
   
-  # Inject an RC style that strips the Windows Wimp engine from these specific widgets.
-  # This returns total background control to standard GTK, eliminating the forced left gutter.
-  rc_style <- "
-    style 'jaw_menu_base' {
-      engine \"\" {}
-      bg[NORMAL] = '#F0F0F0'
+  #Inject RC string to permanently collapse the invisible left gutter.
+  #This allows our EventBox to stretch entirely edge-to-edge.
+  RGtk2::gtkRcParseString("
+    style 'jaw_menu_flush' {
+      GtkMenuItem::toggle-spacing = 0
+      GtkMenuItem::indicator-size = 0
     }
-    style 'jaw_light_item' {
-      engine \"\" {}
-      bg[NORMAL] = '#F0F0F0'
-      bg[PRELIGHT] = '#CCE8FF'
-      fg[NORMAL] = '#000000'
-      fg[PRELIGHT] = '#000000'
-      text[NORMAL] = '#000000'
-      text[PRELIGHT] = '#000000'
-    }
-    style 'jaw_dark_item' {
-      engine \"\" {}
-      bg[NORMAL] = '#C8C8C8'
-      bg[PRELIGHT] = '#CCE8FF'
-      fg[NORMAL] = '#000000'
-      fg[PRELIGHT] = '#000000'
-      text[NORMAL] = '#000000'
-      text[PRELIGHT] = '#000000'
-    }
-    widget '*jaw_context_menu*' style 'jaw_menu_base'
-    widget '*jaw_context_menu*.*' style 'jaw_light_item'
-    widget '*jaw_context_menu*.jaw_dark_item' style 'jaw_dark_item'
-    widget '*jaw_context_menu*.jaw_dark_item.*' style 'jaw_dark_item'
-  "
-  RGtk2::gtkRcParseString(rc_style)
-
-  # Name the parent menu so it gets caught by the RC wildcard string above
-  if (!is.null(obj[[parent_name]])) {
-    RGtk2::gtkWidgetSetName(obj[[parent_name]], "jaw_context_menu")
-  }
-
+    widget_class '*GtkMenuItem*' style 'jaw_menu_flush'
+  ")
+  
   if (is.list(my_list)) {
     menu_dirs <- names(my_list)
     for (my_sub_str in menu_dirs) {
       my_sub_str_name <- my_sub_str
       item_d_name <- paste0(parent_name, "|", my_sub_str_name)
-      
-      menu_item <- RGtk2::gtkMenuItem(label = my_sub_str_name)
-      
-      # Assign the native style names
-      if (my_sub_str_name %in% dark_items) {
-        RGtk2::gtkWidgetSetName(menu_item, "jaw_dark_item")
-      } else {
-        RGtk2::gtkWidgetSetName(menu_item, "jaw_light_item")
-      }
-      
-      obj$items[[item_d_name]] <- menu_item
+      obj$items[[item_d_name]] <- RGtk2::gtkMenuItem(label = my_sub_str_name)
       RGtk2::gtkMenuShellAppend(obj[[parent_name]], obj$items[[item_d_name]])
-      
-      # Create submenu and tag it to propagate the Wimp-free zone
       obj[[item_d_name]] <- RGtk2::gtkMenu()
-      RGtk2::gtkWidgetSetName(obj[[item_d_name]], "jaw_context_menu")
-      
       RGtk2::gtkMenuItemSetSubmenu(obj$items[[item_d_name]], obj[[item_d_name]])
       obj <- z__create_menu_from_list(
         obj, item_d_name,
@@ -85,14 +44,47 @@ z__create_menu_from_list <- function(obj, parent_name, my_list) {
       my_sub_str_name <- my_sub_str
       item_d_name <- paste0(parent_name, "|", my_sub_str_name)
       
-      menu_item <- RGtk2::gtkMenuItem(label = my_sub_str_name)
+      #Create an empty menu item.
+      menu_item <- RGtk2::gtkMenuItemNew()
       
-      # Assign the native style names
+      #Create an event box to control background rendering completely.
+      eb <- RGtk2::gtkEventBoxNew()
+      RGtk2::gtkWidgetSetHasWindow(eb, TRUE)
+      
+      #Create the text label and align it left.
+      lbl <- RGtk2::gtkLabelNew(my_sub_str_name)
+      lbl$xalign <- 0
+      
+      #Add visual padding inside the EventBox so it mimics native menus.
+      lbl["xpad"] <- 25
+      lbl["ypad"] <- 4
+      
+      #Pack them together.
+      RGtk2::gtkContainerAdd(eb, lbl)
+      RGtk2::gtkContainerAdd(menu_item, eb)
+      
+      #Determine the colors for the block.
       if (my_sub_str_name %in% dark_items) {
-        RGtk2::gtkWidgetSetName(menu_item, "jaw_dark_item")
+        c_norm <- RGtk2::gdkColorParse("#C8C8C8")$color
+        c_hov <- RGtk2::gdkColorParse("#91C9F7")$color
       } else {
-        RGtk2::gtkWidgetSetName(menu_item, "jaw_light_item")
+        c_norm <- RGtk2::gdkColorParse("#F0F0F0")$color
+        c_hov <- RGtk2::gdkColorParse("#91C9F7")$color
       }
+      
+      #Set the normal background.
+      RGtk2::gtkWidgetModifyBg(eb, "normal", c_norm)
+      
+      #Manually capture hover states to swap colors with solid paint.
+      RGtk2::gSignalConnect(menu_item, "select", function(widget, data) {
+        RGtk2::gtkWidgetModifyBg(data$eb, "normal", data$c_hov)
+        return(FALSE)
+      }, data = list(eb = eb, c_hov = c_hov))
+      
+      RGtk2::gSignalConnect(menu_item, "deselect", function(widget, data) {
+        RGtk2::gtkWidgetModifyBg(data$eb, "normal", data$c_norm)
+        return(FALSE)
+      }, data = list(eb = eb, c_norm = c_norm))
       
       obj$items[[item_d_name]] <- menu_item
       RGtk2::gtkMenuShellAppend(obj[[parent_name]], obj$items[[item_d_name]])
