92e892df12e8b84a046fe7f637f544c3c0754e93
elderneff <66916096+elderneff@users.noreply.github.com>
2026-07-25 11:48:47 -0600

    Alternating colors in right click menu

--- R/z__create_menu_from_list.R
+++ R/z__create_menu_from_list.R
@@ -7,6 +7,14 @@
 #' @return TODO
 
 z__create_menu_from_list <- function(obj, parent_name, my_list) {
+  #Define items that should have the darker grey background.
+  dark_items <- c(
+    "if then", "if then do",
+    "Column full", "Column filtered", "Column Wide",
+    "Add Column to Main Filter Exclude", "Add to Main Filter Exclude",
+    "Add Bucket to Main Filter Exclude"
+  )
+  
   if (is.list(my_list)) {
     menu_dirs <- names(my_list)
     for (my_sub_str in menu_dirs) {
@@ -26,12 +34,46 @@ z__create_menu_from_list <- function(obj, parent_name, my_list) {
       my_sub_str_name <- my_sub_str
       item_d_name <- paste0(parent_name, "|", my_sub_str_name)
       
-      menu_item <- RGtk2::gtkMenuItem(label = my_sub_str_name)
+      #1. Create an empty menu item.
+      menu_item <- RGtk2::gtkMenuItemNew()
       
-      #Assign the dark grey style to specific items
-      if (my_sub_str_name %in% c("if then", "if then do")) {
-        RGtk2::gtkWidgetSetName(menu_item, "MenuItemDark")
+      #2. Create an event box to control background rendering completely.
+      eb <- RGtk2::gtkEventBoxNew()
+      
+      #3. Create the text label and align it left.
+      lbl <- RGtk2::gtkLabelNew(my_sub_str_name)
+      lbl$xalign <- 0
+      
+      #Add visual padding to mimic a native menu item.
+      lbl["xpad"] <- 16
+      lbl["ypad"] <- 4
+      
+      #4. Pack them together.
+      RGtk2::gtkContainerAdd(eb, lbl)
+      RGtk2::gtkContainerAdd(menu_item, eb)
+      
+      #5. Determine the colors for the block.
+      if (my_sub_str_name %in% dark_items) {
+        c_norm <- RGtk2::gdkColorParse("#D0D0D0")$color
+        c_hov <- RGtk2::gdkColorParse("#B0B0B0")$color
+      } else {
+        c_norm <- RGtk2::gdkColorParse("#F0F0F0")$color
+        c_hov <- RGtk2::gdkColorParse("#E5E5E5")$color
       }
+      
+      #Set the normal background.
+      RGtk2::gtkWidgetModifyBg(eb, "normal", c_norm)
+      
+      #6. Manually capture hover states to swap colors.
+      RGtk2::gSignalConnect(menu_item, "select", function(widget, data) {
+        RGtk2::gtkWidgetModifyBg(data$eb, "normal", data$c_hov)
+        return(FALSE)
+      }, data = list(eb = eb, c_hov = c_hov))
+      
+      RGtk2::gSignalConnect(menu_item, "deselect", function(widget, data) {
+        RGtk2::gtkWidgetModifyBg(data$eb, "normal", data$c_norm)
+        return(FALSE)
+      }, data = list(eb = eb, c_norm = c_norm))
       
       obj$items[[item_d_name]] <- menu_item
       RGtk2::gtkMenuShellAppend(obj[[parent_name]], obj$items[[item_d_name]])
