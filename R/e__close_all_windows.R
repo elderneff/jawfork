6d924f69a1bfeb9c74f116254141dd71f49e8d57
elderneff <66916096+elderneff@users.noreply.github.com>
2026-08-26 17:10:00 -0600

    Settings for startup freeze and startup layout

--- R/e__close_all_windows.R
+++ R/e__close_all_windows.R
@@ -35,7 +35,7 @@ e__close_all_windows <- function(session_name,outer_env=totem) {
     if (is.list(disk_settings)) {
       
       # 1. Sync global checkboxes and keybinds
-      for (setting_name in c("maximize", "ctrlshift", "columnlabel", "columnunique", "professionalloading", "table_events", "select_everything", "default_freeze", "fixed_layout", "pending_fixed_layout")) {
+      for (setting_name in c("maximize", "ctrlshift", "columnlabel", "columnunique", "professionalloading", "table_events", "select_everything", "default_freeze", "startup_layout", "pending_startup_layout")) {
         # If this session DID NOT change the setting from its initial state...
         if (identical(outer_env$settings_list[[setting_name]], outer_env[[session_name]]$initial_settings_snapshot[[setting_name]])) {
           # ...safely inherit the latest version from the disk
