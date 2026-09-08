787c368603790d0f1cb04e98650191b5cc5d017f
elderneff <66916096+elderneff@users.noreply.github.com>
2026-09-07 20:43:06 -0600

    Replace alternating colors in context menus with multiple levels

--- R/z__create_menu_from_list.R
+++ R/z__create_menu_from_list.R
@@ -1,33 +1,23 @@
-#' z__create_menu_from_list
-#'
-#' @param obj TODO
-#' @param parent_name TODO
-#' @param my_list TODO
-#'
-#' @return TODO
-
+#Creates a nested GTK menu structure from a list.
 z__create_menu_from_list <- function(obj, parent_name, my_list) {
-  for (item_name in my_list[[parent_name]]) {
-    node_name <- paste0(parent_name, "|", item_name)
+  for (category_name in names(my_list)) {
+    #Create the parent category item.
+    category_item <- RGtk2::gtkMenuItem(label = category_name)
+    RGtk2::gtkMenuShellAppend(obj[[parent_name]], category_item)
     
-    #Create standard GTK menu item without custom color formatting.
-    obj$item[[node_name]] <- RGtk2::gtkMenuItem(label = item_name)
+    #Create the submenu to hold the actual actions.
+    sub_menu <- RGtk2::gtkMenu()
+    RGtk2::gtkMenuItemSetSubmenu(category_item, sub_menu)
     
-    if (node_name %in% names(my_list)) {
-      obj$item[[node_name]][["submenu"]] <- RGtk2::gtkMenu()
-      obj <- z__create_menu_from_list(obj, node_name, my_list)
-      RGtk2::gtkMenuItemSetSubmenu(obj$item[[node_name]], obj$item[[node_name]][["submenu"]])
-    } else {
-      obj$end_nodes <- c(obj$end_nodes, node_name)
-    }
-    
-    #Attach the item to the appropriate parent menu level.
-    if (parent_name == "base") {
-      RGtk2::gtkMenuShellAppend(obj[[parent_name]], obj$item[[node_name]])
-    } else {
-      RGtk2::gtkMenuShellAppend(obj$item[[parent_name]][["submenu"]], obj$item[[node_name]])
+    for (item_name in my_list[[category_name]]) {
+      #Create the clickable action item.
+      action_item <- RGtk2::gtkMenuItem(label = item_name)
+      RGtk2::gtkMenuShellAppend(sub_menu, action_item)
+      
+      #Store a reference so event handlers and settings can access it later.
+      end_node <- paste0(parent_name, "|", category_name, "|", item_name)
+      obj$items[[end_node]] <- action_item
     }
   }
-  
   return(obj)
 }
