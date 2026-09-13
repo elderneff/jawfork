fcbf5160a79d1293f45d6488c942913f776198da
elderneff <66916096+elderneff@users.noreply.github.com>
2026-09-12 21:13:16 -0600

    Undo alternating colors for right click menus

--- R/z__create_menu_from_list.R
+++ R/z__create_menu_from_list.R
@@ -14,15 +14,31 @@ z__create_menu_from_list <- function(obj, parent_name, my_list) {
     for (my_sub_str in menu_dirs) {
       my_sub_str_name <- my_sub_str
       item_d_name <- paste0(parent_name, "|", my_sub_str_name)
+      
       obj$items[[item_d_name]] <- RGtk2::gtkMenuItem(label = my_sub_str_name)
       RGtk2::gtkMenuShellAppend(obj[[parent_name]], obj$items[[item_d_name]])
+      
       obj[[item_d_name]] <- RGtk2::gtkMenu()
       RGtk2::gtkMenuItemSetSubmenu(obj$items[[item_d_name]], obj[[item_d_name]])
       
-      #You will also need to remove any downstream if-statements in your loop.
-      #Remove the logic that checks if the item is in dark_items or conditional_dark_items.
-      
       obj <- z__create_menu_from_list(obj, item_d_name, my_list[[my_sub_str]])
+    }
+  } else {
+    menu_items <- my_list
+    for (my_sub_str in menu_items) {
+      my_sub_str_name <- my_sub_str
+      
+      if (my_sub_str_name == "-") {
+        #Add separators.
+        item_d_name <- paste0(parent_name, "|", my_sub_str_name, runif(1))
+        obj$items[[item_d_name]] <- RGtk2::gtkSeparatorMenuItem()
+        RGtk2::gtkMenuShellAppend(obj[[parent_name]], obj$items[[item_d_name]])
+      } else {
+        #Add standard menu items.
+        item_d_name <- paste0(parent_name, "|", my_sub_str_name)
+        obj$items[[item_d_name]] <- RGtk2::gtkMenuItem(label = my_sub_str_name)
+        RGtk2::gtkMenuShellAppend(obj[[parent_name]], obj$items[[item_d_name]])
+      }
     }
   }
   return(obj)
